b64baa3e71e15cafe7282ee555ec6c0394072974
Richard Wohlbold <rigewo02@gmail.com>
2018-07-04 10:34:01 +0200

    Turn echo off in run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
+@echo off
+
 docker run --rm --mount type=bind,source="%cd%/certs",target=/openvpn --mount type=bind,source="%cd%\easy-rsa",target=/etc/easy-rsa vpn-docker:latest /data/generate_keys.sh %*
 rem docker run --rm -it --mount type=bind,source="%cd%/certs",target=/openvpn --mount type=bind,source="%cd%\easy-rsa",target=/etc/easy-rsa vpn-docker:latest %*
